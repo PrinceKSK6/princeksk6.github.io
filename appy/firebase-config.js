046dc48a32b2328ac15b820135d00f39f49fdb4b
Prince KSK <41015899+PrinceKSK6@users.noreply.github.com>
2025-06-22 22:45:47 +0530

    Update firebase-config.js

--- appy/firebase-config.js
+++ appy/firebase-config.js
@@ -1,4 +1,12 @@
-// Replace this with your actual Firebase config
-const firebaseConfig = { /* your config */ };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDoMnGC8b0rql4IMC7qSYXsQoTsT1z0i4A",
+  authDomain: "driver-manager-a242b.firebaseapp.com",
+  projectId: "driver-manager-a242b",
+  storageBucket: "driver-manager-a242b.firebasestorage.app",
+  messagingSenderId: "659404728909",
+  appId: "1:659404728909:web:b59a4d54ac8a3a8bb08a04",
+  measurementId: "G-4E9TTQ6CDS"
+};
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
